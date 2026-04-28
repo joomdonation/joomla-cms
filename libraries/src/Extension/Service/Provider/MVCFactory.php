92e2decb4dd192529a814cf7556de83d28029b20
Tuan Pham Ngoc <github@joomdonation.com>
2026-04-28 16:00:07 +0700

    Make MVCFactory service provider extendable

--- libraries/src/Extension/Service/Provider/MVCFactory.php
+++ libraries/src/Extension/Service/Provider/MVCFactory.php
@@ -4,13 +4,15 @@
  * Joomla! Content Management System
  *
  * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @license        GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Joomla\CMS\Extension\Service\Provider;
 
 use Joomla\CMS\Cache\CacheControllerFactoryInterface;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormFactoryInterface;
+use Joomla\CMS\Http\HttpFactoryInterface;
 use Joomla\CMS\Mail\MailerFactoryInterface;
 use Joomla\CMS\MVC\Factory\ApiMVCFactory;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
@@ -67,22 +69,58 @@ class MVCFactory implements ServiceProviderInterface
         $container->set(
             MVCFactoryInterface::class,
             function (Container $container) {
-                if (\Joomla\CMS\Factory::getApplication()->isClient('api')) {
-                    $factory = new ApiMVCFactory($this->namespace);
-                } else {
-                    $factory = new \Joomla\CMS\MVC\Factory\MVCFactory($this->namespace);
-                }
+                $factory = $this->createMVCFactory();
 
-                $factory->setFormFactory($container->get(FormFactoryInterface::class));
-                $factory->setDispatcher($container->get(DispatcherInterface::class));
-                $factory->setDatabase($container->get(DatabaseInterface::class));
-                $factory->setSiteRouter($container->get(SiteRouter::class));
-                $factory->setCacheControllerFactory($container->get(CacheControllerFactoryInterface::class));
-                $factory->setUserFactory($container->get(UserFactoryInterface::class));
-                $factory->setMailerFactory($container->get(MailerFactoryInterface::class));
+                $this->injectServicesIntoFactory($factory, $container);
 
                 return $factory;
             }
         );
+    }
+
+    /**
+     * Get namespace
+     *
+     * @return string
+     */
+    protected function getNamespace()
+    {
+        return $this->namespace;
+    }
+
+    /**
+     * Create MVC Factory
+     *
+     * @return ApiMVCFactory|\Joomla\CMS\MVC\Factory\MVCFactory
+     *
+     * @throws \Exception
+     */
+    protected function createMVCFactory()
+    {
+        if (Factory::getApplication()->isClient('api')) {
+            return new ApiMVCFactory($this->namespace);
+        }
+
+        return new \Joomla\CMS\MVC\Factory\MVCFactory($this->namespace);
+    }
+
+    /**
+     * Inject services from container to MVCFactory
+     *
+     * @param   MVCFactory  $factory
+     * @param   Container   $container
+     *
+     * @return void
+     */
+    protected function injectServicesIntoFactory(\Joomla\CMS\MVC\Factory\MVCFactory $factory, Container $container)
+    {
+        $factory->setFormFactory($container->get(FormFactoryInterface::class));
+        $factory->setDispatcher($container->get(DispatcherInterface::class));
+        $factory->setDatabase($container->get(DatabaseInterface::class));
+        $factory->setSiteRouter($container->get(SiteRouter::class));
+        $factory->setCacheControllerFactory($container->get(CacheControllerFactoryInterface::class));
+        $factory->setUserFactory($container->get(UserFactoryInterface::class));
+        $factory->setMailerFactory($container->get(MailerFactoryInterface::class));
+        $factory->setHttpFactory($container->get(HttpFactoryInterface::class));
     }
 }
